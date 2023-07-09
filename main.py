9424f226b7eb673e9d31d5f109612d2dc076b302
Luiz Guilherme <luizgmelo_@hotmail.com>
2023-07-08 23:35:36 -0300

    first version calculator python

--- main.py
+++ main.py
@@ -2,3 +2,21 @@
 # operation
 # another number
 # result
+firstNumber = int(input("Type a number: "))
+operation = str(input("Type the operation: "))
+secondNumber = int(input("Type another number: "))
+
+if operation == "+":
+    result = firstNumber + secondNumber
+    print(f"{firstNumber} + {secondNumber} = {result}")
+elif operation == "-":
+    result = firstNumber - secondNumber
+    print(f"{firstNumber} - {secondNumber} = {result}")
+elif operation == "*" or operation == "x":
+    result = firstNumber * secondNumber
+    print(f"{firstNumber} x {secondNumber} = {result}")
+elif operation == "/":
+    result = firstNumber / secondNumber
+    print(f"{firstNumber} / {secondNumber} = {result}")
+else:
+    print("Value is not valid, Try Again")
